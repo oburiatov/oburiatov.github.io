f48873c8852abb930f3f68d70b194c58b61b1262
Alexei <alexei.buriatov@gmail.com>
2020-11-22 18:53:27 +0200

    Editing Json file

--- Bookshop/JS/script.js
+++ Bookshop/JS/script.js
@@ -3,7 +3,7 @@ async function getResponse()
 var response = await fetch('https://raw.githubusercontent.com/alexeiburiatov/alexeiburiatov.github.io/master/Bookshop/JSON/data.json')
 var content =  await response.json()
 console.log(content);
-console.log(content["profileName"]);
+console.log(content);
 
 }
 
