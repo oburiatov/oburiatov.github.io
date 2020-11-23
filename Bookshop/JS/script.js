281ddb8b8056d4b282c6d0c314ed5c5aed279669
Alexei <alexei.buriatov@gmail.com>
2020-11-23 17:40:24 +0200

    Modifying Json file

--- Bookshop/JS/script.js
+++ Bookshop/JS/script.js
@@ -2,8 +2,15 @@ async function getResponse()
 {
 var response = await fetch('https://my-json-server.typicode.com/alexeiburiatov/alexeiburiatov.github.io/db')
 var content =  await response.json();
-if(content["books"][0]["section"]==2)
+if(content["books"][0]["sales"]==1)
 {
+    var img = document.createElement("IMG");
+img.src=content["books"][0]["img-path"];
+var pc= document.getElementById("insertimg4");
+pc.appendChild(img);
+
+
+
     console.log(content["books"][0]["sales"]);
 }
 
