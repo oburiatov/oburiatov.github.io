11ad2c1170847de0fe3f8ae2780b8db4471dc5c0
Alexei <alexei.buriatov@gmail.com>
2020-11-27 22:42:03 +0200

    JSON

--- JS/view/index.js
+++ JS/view/index.js
@@ -1,21 +1,21 @@
-import TemplateProcessor from './templateProcessor.js'
-import Client from './client.js'
-import Router from './router.js'
+// import TemplateProcessor from './templateProcessor.js'
+// import Client from './client.js'
+// import Router from './router.js'
 
-const router = new Router();
-const templateProcessor= new TemplateProcessor();
-const client= new Client();
+// const router = new Router();
+// const templateProcessor= new TemplateProcessor();
+// const client= new Client();
 
-const {viewName, endpointName } = router.getCurrentState();
+// const {viewName, endpointName } = router.getCurrentState();
 
-let view;
-import (view/$(viewName).js)
-    .then ((viewModule)=> {
-    view= viewModule.default;
-    return client.getData(endpointName);
-    })
-    .then ((data) =>{
-        templateProcessor.render((view(data)));
-    });
+// let view;
+// import (view/$(viewName).js)
+//     .then ((viewModule)=> {
+//     view= viewModule.default;
+//     return client.getData(endpointName);
+//     })
+//     .then ((data) =>{
+//         templateProcessor.render((view(data)));
+//     });
 
     
