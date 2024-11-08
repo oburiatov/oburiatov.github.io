fcdbb57341d70db5e95878adf191120a72cd041f
Oleksii Buriatov <oleksii.buriatov@gmail.com>
2024-11-08 21:06:02 +0200

    Update current hash checking process

--- Bookshop/JS/view/router.js
+++ Bookshop/JS/view/router.js
@@ -5,34 +5,35 @@ class Router {
         let unique_url;
         let section;
         let catalog;
-        let curr_hash=window.location.hash
-        if(curr_hash.split('#')[1].includes('/')&&curr_hash.split('#')[1].split('/')[0]!='') {
-            unique_url=curr_hash.split('#')[1].split('/')[1]
-            section=curr_hash.split('#')[1].split('/')[0]
+        let curr_hash = window.location.hash;
+        let hashValue = curr_hash.split('#')[1];
+        if (hashValue.includes('/') && hashValue.split('/')[0] !== '') {
+            unique_url = curr_hash.split('#')[1].split('/')[1]
+            section = curr_hash.split('#')[1].split('/')[0]
             switch (curr_hash.split('#')[1].split('/')[1]) {
                 case 'self-development':
                     viewName = 'Self-developmentPage'
                     endpointName = 'books'
-                    catalog= 'self-development'
-                    section='catalog'
+                    catalog = 'self-development'
+                    section = 'catalog'
                     break;
                 case 'motivational':
                     viewName = 'MotivationalPage'
                     endpointName = 'books'
-                    catalog= 'motivational'
-                    section='catalog'
+                    catalog = 'motivational'
+                    section = 'catalog'
                     break;
                 case 'psychological':
                     viewName = 'PsychologicalPage'
                     endpointName = 'books'
-                    catalog= 'psychological'
-                    section='catalog'
+                    catalog = 'psychological'
+                    section = 'catalog'
                     break;
-                    case 'sales':
+                case 'sales':
                     viewName = 'salesPage'
                     endpointName = 'books'
-                    catalog= 'sales'
-                    section='catalog'
+                    catalog = 'sales'
+                    section = 'catalog'
                     break;
 
                 default:
@@ -40,55 +41,54 @@ class Router {
             }
 
         }
-        else if(curr_hash.split('#')[1].includes('/')&&curr_hash.split('#')[1].length==1){
+        else if (curr_hash.split('#')[1].includes('/') && curr_hash.split('#')[1].length == 1) {
             switch (curr_hash.split('#')[1]) {
                 case '/':
                     viewName = 'mainPage'
-                    endpointName='db'
+                    endpointName = 'db'
                     break;
                 case 'Self-development':
                     viewName = 'Self-developmentPage'
-                    endpointName='books'
+                    endpointName = 'books'
                     break;
                 case 'Motivational':
                     viewName = 'MotivationalPage'
-                    endpointName='books'
+                    endpointName = 'books'
                     break;
                 case 'Psychological':
                     viewName = 'PsychologicalPage'
-                    endpointName='books'
+                    endpointName = 'books'
                     break;
 
                 default:
                     break;
-        }
+            }
 
         }
-        else if(curr_hash.split('#')[1].includes('/')&&curr_hash.split('#')[1].length!=1&&curr_hash.split('#')[1].split('/')[1]!='')
-        {
-            unique_url=curr_hash.split('#')[1].split('/')[1]
-            section=curr_hash.split('#')[1].split('/')[0]
+        else if (curr_hash.split('#')[1].includes('/') && curr_hash.split('#')[1].length != 1 && curr_hash.split('#')[1].split('/')[1] != '') {
+            unique_url = curr_hash.split('#')[1].split('/')[1]
+            section = curr_hash.split('#')[1].split('/')[0]
             viewName = 'productDescription'
             endpointName = 'books'
         }
-        return{
+        return {
             viewName,
             endpointName,
             section,
             unique_url
         };
     }
-    getCurrentStatePage(){
-        let endpointName='books'
-        return {endpointName};
+    getCurrentStatePage() {
+        let endpointName = 'books'
+        return { endpointName };
     }
-    getCurrentStatePageDescription(){
+    getCurrentStatePageDescription() {
 
-        let viewName='productDescription'
+        let viewName = 'productDescription'
         let endpointName = 'books';
-        let curr_hash=window.location.hash
-        let unique_url=curr_hash.split('#')[1].split('/')[1]
-        return {viewName, endpointName, unique_url};
+        let curr_hash = window.location.hash
+        let unique_url = curr_hash.split('#')[1].split('/')[1]
+        return { viewName, endpointName, unique_url };
 
     }
 
